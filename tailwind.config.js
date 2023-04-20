eb36c97bfe06ecfeaea39d1c319c010e93e268c1
Diego Gallegos <diego.gallegos.14@sansano.usm.cl>
2023-04-20 16:00:53 -0400

    added tailwind to project

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'shiny-green': '#10FF00',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
